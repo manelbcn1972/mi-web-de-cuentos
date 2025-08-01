55496017907988667dfff5b930a16c3888fb9f5c
manelbcn1972 <mibanez.1972@gmail.com>
2025-08-02 01:19:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.googleAiApiKey = 'AIzaSyBLsNvpfVDDxRSfDMo3LHOKkxYA2b-KFgAI'';
+window.googleAiApiKey = 'AIzaSyBLsNvpfVDDxRSfDMo3LHOKkxYA2b-KFgAI';
 window.googleClientId = '90799928498-epmcc1ufu2bsss7p3j9ni0tpavkrd8dp.apps.googleusercontent.com';
