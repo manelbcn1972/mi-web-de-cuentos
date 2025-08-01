e908660bd56ad257553edb9fbde16788d191fff1
manelbcn1972 <mibanez.1972@gmail.com>
2025-08-02 00:56:57 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+   ```javascript
+   window.googleAiApiKey = 'AIzaSyBLsNvpfVDDxRSfDMo3LHOKkxYA2b-KFgAI';
+   window.googleClientId = '90799928498-epmcc1ufu2bsss7p3j9ni0tpavkrd8dp.apps.googleusercontent.com';
